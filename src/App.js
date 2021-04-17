f3d97f1722ff9f4323b591df83e16ed59f2c480c
Mahfuzul-Islam55 <shimulsust999@gmail.com>
2021-04-17 16:23:16 +0600

    fetching data from the api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+
+import React, { Component,Fragment } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import ClassCounter from './components/ClassCounter';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
 import HooksCounter from './components/HooksCounter';
+import {fetchData} from './api/getUrl';
 
-function App() {
-  return ( 
-    
-    <div>
-      <HooksCounter></HooksCounter>
-    </div>
-    
-  );
+
+
+class App extends Component {
+
+  async componentDidMount(){
+     const data=await fetchData();
+     console.log(data)
+  }
+  render() {
+    return (
+     <Fragment>
+       <Cards></Cards>
+      <Chart></Chart>
+      <CountryPicker></CountryPicker>
+     </Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -5,6 +5,7 @@ const HooksCounter = () => {
     const [name,setName]=useState({firstName:'',lastName:''})
     return (
         <div>
+            <p>okkkkkkkk</p>
             <input type="text" value={name.firstName} onChange={e=>setName({...name,firstName:e.target.value})}></input>
             <input type="text" value={name.lastName} onChange={e=>setName({...name,lastName:e.target.value})}></input>
 
